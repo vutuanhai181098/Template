6864468955644e67b4bef88db8a1cef843e44497
Tuan Hai <vutuanhai.18101998@gmail.com>
2023-12-23 10:42:17 +0700

    update

--- Template-ecommerce/main.js
+++ Template-ecommerce/main.js
@@ -47,130 +47,130 @@ showHidePassEl.forEach(e => {
 </div>
 </div>
 </div> */}
-document.addEventListener('DOMContentLoaded', function() {
-    let load = false;
-    function bannerSliderSlick() {
-        $(`[rel-script="product-slider"]`).slick({
-            slidesToShow: 5,
-            slidesToScroll: 5,
-            centerPadding: '30px',
-            arrows: true,
-            dots: false,
-            loop: true,
-            infinite: true,
-            speed: 300,
-            gap: 32,
-            autoplay: true,
-            autoplaySpeed: 4000,
-            pauseOnHover: true
-        });
-    }
+// document.addEventListener('DOMContentLoaded', function() {
+//     let load = false;
+//     function bannerSliderSlick() {
+//         $(`[rel-script="product-slider"]`).slick({
+//             slidesToShow: 5,
+//             slidesToScroll: 5,
+//             centerPadding: '30px',
+//             arrows: true,
+//             dots: false,
+//             loop: true,
+//             infinite: true,
+//             speed: 300,
+//             gap: 32,
+//             autoplay: true,
+//             autoplaySpeed: 4000,
+//             pauseOnHover: true
+//         });
+//     }
 
-    function getProductByCollectionId(container,ids) {
-        if (!ids) return;
-        const API_URL =
-            `${window.location.origin}/get-product-by-collection-id?collections=${ids}`;
-        $.ajax({
-            url: API_URL,
-            type: "GET",
-            cache: false,
-        }).done(function (res) {
-            if (res.success) {
-                if (res.html != '') {
-                    container.html(res.html);
-                    bannerSliderSlick();
-                }
-            }
-        })
-    }
-
-
-    jQuery(document).ready(function($) {
-        let loading = `
-            <div class="grid__column product-loading" style="position: relative">
-<div class="product-grid product">
-<div class="product-grid__thumbnail">
-    <div class="product-grid__image"></div>
-</div>
-<div class="product-grid__content" data-collection-tag="">
-    <h3 class="product-grid__title">
-    </h3>
-    <div class="product-grid__prices">
-    </div>
-</div>
-</div>
-</div>
-            <div class="grid__column product-loading" style="position: relative">
-<div class="product-grid product">
-<div class="product-grid__thumbnail">
-    <div class="product-grid__image"></div>
-</div>
-<div class="product-grid__content" data-collection-tag="">
-    <h3 class="product-grid__title">
-    </h3>
-    <div class="product-grid__prices">
-    </div>
-</div>
-</div>
-</div>
-            <div class="grid__column product-loading" style="position: relative">
-<div class="product-grid product">
-<div class="product-grid__thumbnail">
-    <div class="product-grid__image"></div>
-</div>
-<div class="product-grid__content" data-collection-tag="">
-    <h3 class="product-grid__title">
-    </h3>
-    <div class="product-grid__prices">
-    </div>
-</div>
-</div>
-</div>
-            <div class="grid__column product-loading" style="position: relative">
-<div class="product-grid product">
-<div class="product-grid__thumbnail">
-    <div class="product-grid__image"></div>
-</div>
-<div class="product-grid__content" data-collection-tag="">
-    <h3 class="product-grid__title">
-    </h3>
-    <div class="product-grid__prices">
-    </div>
-</div>
-</div>
-</div>
-        `
-        let loadContent = false;
-        let seo_alias = 'san-pham-moi';
-        // $(window).on('scroll',function (e) {
-            // if(!loadContent) {
-                indexLoadMoreCollections(seo_alias, ['sub_items=','limit=30','flatten=true','random_focus=true'], 1, function (res) {
-                    const {html, success, is_next_page} = res;
-                    if (success && html != '') {
-                        $('[rel-script="product-slider"]').html(html);
-                        bannerSliderSlick();
-                    }
-                })
-                loadContent = true
-            // }
-        // })
+//     function getProductByCollectionId(container,ids) {
+//         if (!ids) return;
+//         const API_URL =
+//             `${window.location.origin}/get-product-by-collection-id?collections=${ids}`;
+//         $.ajax({
+//             url: API_URL,
+//             type: "GET",
+//             cache: false,
+//         }).done(function (res) {
+//             if (res.success) {
+//                 if (res.html != '') {
+//                     container.html(res.html);
+//                     bannerSliderSlick();
+//                 }
+//             }
+//         })
+//     }
 
 
-        $(document).on('click','[rel-script="tab-featured-products"]',function (e){
-            e.preventDefault();
-            const _this = $(this);
-            const seo_alias = _this.attr('data-value');
-            _this.siblings().removeClass('active');
-            _this.addClass('active');
-            $(`[rel-script="product-slider"]`).slick('unslick');
-            $(`[rel-script="product-slider"]`).html(loading);
-            indexLoadMoreCollections(`${seo_alias}`, ['sub_items=','limit=15','flatten=true','random_focus=true'], 1, function (res) {
-                const {html, success, is_next_page} = res;
-                if (success && html != '') {
-                    $(`[rel-script="product-slider"]`).html(html);
-                    bannerSliderSlick();
-                }
-            })
-        });
-    });
-});
+//     jQuery(document).ready(function($) {
+//         let loading = `
+//             <div class="grid__column product-loading" style="position: relative">
+// <div class="product-grid product">
+// <div class="product-grid__thumbnail">
+//     <div class="product-grid__image"></div>
+// </div>
+// <div class="product-grid__content" data-collection-tag="">
+//     <h3 class="product-grid__title">
+//     </h3>
+//     <div class="product-grid__prices">
+//     </div>
+// </div>
+// </div>
+// </div>
+//             <div class="grid__column product-loading" style="position: relative">
+// <div class="product-grid product">
+// <div class="product-grid__thumbnail">
+//     <div class="product-grid__image"></div>
+// </div>
+// <div class="product-grid__content" data-collection-tag="">
+//     <h3 class="product-grid__title">
+//     </h3>
+//     <div class="product-grid__prices">
+//     </div>
+// </div>
+// </div>
+// </div>
+//             <div class="grid__column product-loading" style="position: relative">
+// <div class="product-grid product">
+// <div class="product-grid__thumbnail">
+//     <div class="product-grid__image"></div>
+// </div>
+// <div class="product-grid__content" data-collection-tag="">
+//     <h3 class="product-grid__title">
+//     </h3>
+//     <div class="product-grid__prices">
+//     </div>
+// </div>
+// </div>
+// </div>
+//             <div class="grid__column product-loading" style="position: relative">
+// <div class="product-grid product">
+// <div class="product-grid__thumbnail">
+//     <div class="product-grid__image"></div>
+// </div>
+// <div class="product-grid__content" data-collection-tag="">
+//     <h3 class="product-grid__title">
+//     </h3>
+//     <div class="product-grid__prices">
+//     </div>
+// </div>
+// </div>
+// </div>
+//         `
+//         let loadContent = false;
+//         let seo_alias = 'san-pham-moi';
+//         // $(window).on('scroll',function (e) {
+//             // if(!loadContent) {
+//                 indexLoadMoreCollections(seo_alias, ['sub_items=','limit=30','flatten=true','random_focus=true'], 1, function (res) {
+//                     const {html, success, is_next_page} = res;
+//                     if (success && html != '') {
+//                         $('[rel-script="product-slider"]').html(html);
+//                         bannerSliderSlick();
+//                     }
+//                 })
+//                 loadContent = true
+//             // }
+//         // })
+
+
+//         $(document).on('click','[rel-script="tab-featured-products"]',function (e){
+//             e.preventDefault();
+//             const _this = $(this);
+//             const seo_alias = _this.attr('data-value');
+//             _this.siblings().removeClass('active');
+//             _this.addClass('active');
+//             $(`[rel-script="product-slider"]`).slick('unslick');
+//             $(`[rel-script="product-slider"]`).html(loading);
+//             indexLoadMoreCollections(`${seo_alias}`, ['sub_items=','limit=15','flatten=true','random_focus=true'], 1, function (res) {
+//                 const {html, success, is_next_page} = res;
+//                 if (success && html != '') {
+//                     $(`[rel-script="product-slider"]`).html(html);
+//                     bannerSliderSlick();
+//                 }
+//             })
+//         });
+//     });
+// });
